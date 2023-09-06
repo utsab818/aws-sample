d0bd539028a61462758dd1ca4e3d43b128567137
Utsab Sapkota <66249507+utsab818@users.noreply.github.com>
2023-09-06 12:16:08 +0530

    add the files for code deploy

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull utsab12312/sample-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 utsab12312/sample-python-flask-app
